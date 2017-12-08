e9b35eccaa0dc227096468578e87f750720f7b1b
Vladislav Volotkovich <volotkovich.2012@mail.ru>
2017-12-08 15:03:45 +0300

    Добавьте файлы проекта.

---
+++ Sportcomplex_application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sportcomplex_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
